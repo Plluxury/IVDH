89d97d145f8e69ed37267af0b079ed9ca2fdd7ba
zabar <zabara2388@gmail.com>
2023-09-04 21:43:30 +0300

    Init

--- index/static/index/js/script.js
+++ index/static/index/js/script.js
@@ -6,38 +6,41 @@ navigator.mediaDevices.getUserMedia({ audio: true})
 
         document.querySelector('#start').addEventListener('click', function(){
             mediaRecorder.start();
+
         });
         let audioChunks = [];
         mediaRecorder.addEventListener("dataavailable",function(event) {
             audioChunks.push(event.data);
+            console.log(audioChunks)
         });
 
         document.querySelector('#stop').addEventListener('click', function(){
             mediaRecorder.stop();
+
         });
 
         mediaRecorder.addEventListener("stop", function() {
-            const audioBlob = new Blob(audioChunks, {
-                type: 'audio/wav'
-            });
+            const audioBlob = new Blob(audioChunks, {type: 'audio/wav' });
+            console.log(audioBlob)
 
             let fd = new FormData();
+
             fd.append('voice', audioBlob);
-            sendVoice(fd);
+
+            const requset = new XMLHttpRequest();
+            requset.open("POST", '/audioAPI');
+            requset.send(fd);
+            // sendVoice(fd)
             audioChunks = [];
         });
     });
 
-async function sendVoice(form) {
-    let promise = await fetch('/audioAPI', {
-        method: 'POST',
-        body: form});
-    if (promise.ok) {
-        let response =  await promise.json();
-        console.log(response.data);
-        let audio = document.createElement('audio');
-        audio.src = response.data;
-        audio.controls = true;
-        audio.autoplay = true;
-    }
-}
+// async function sendVoice(form) {
+//     const body = JSON.stringify(Object.fromEntries(form));
+//     let data = await fetch(URL, {
+//         method: 'POST',
+//         body
+//     });
+//     let response = await data.json();
+//
+// }
--- index/views.py
+++ index/views.py
@@ -6,14 +6,27 @@ from django.views.decorators.csrf import csrf_exempt
 from django.http import JsonResponse
 
 
+
 def index(request):
     return render(request, 'index/index.html')
 
 
 @csrf_exempt
 def audio(request):
-    name = ''
-    if request.POST:
-        name = json.loads(request.body)
+    if request.method == "POST":
+        # create a form instance and populate it with data from the request:
+        form = NameForm(request.POST)
+        # check whether it's valid:
+        if form.is_valid():
+            # process the data in form.cleaned_data as required
+            # ...
+            # redirect to a new URL:
+            return HttpResponseRedirect("/audioAPI/")
 
-    return JsonResponse({'name': name})
+    # if a GET (or any other method) we'll create a blank form
+    else:
+        form = NameForm()
+    return render(request, "name.html", {"form": form})
+
+
+
