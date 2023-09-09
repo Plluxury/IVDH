5da285efdc370ad61c9a41b87b2ded94dfce158f
Morozov Artem <easkae1337@gmail.com>
2023-09-09 14:43:32 +0300

    added audio->text/text->audio transform

--- index/views.py
+++ index/views.py
@@ -5,7 +5,8 @@ from django.http import HttpResponse
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 from json import dumps
-
+import speech_recognition as sr
+from gtts import gTTS
 
 def index(request):
     return render(request, 'index/index.html')
@@ -21,6 +22,15 @@ def audio(request):
             with open('index/static/index/media/audio.wav', 'wb') as destination:
                 for chunk in audio_file.chunks():
                     destination.write(chunk)
+
+            r = sr.Recognizer()
+            with sr.AudioFile('index/static/index/media/audio.wav') as source:
+                audio_data = r.record(source)
+                request = r.recognize_google(audio_data, language="ru-RU")
+                
+                response = gTTS(text=request, lang="ru", slow=False)
+                response.save("answer.wav")
+
 
     return JsonResponse({'message': 'Audio received and saved successfully.'})
 
