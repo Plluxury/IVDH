432fa2e0b70851895ed8393e5988e3fba9fdc39d
Pluxury <zabara2388@gmail.com>
2023-09-10 06:55:04 +0300

    Change html/css

--- index/views.py
+++ index/views.py
@@ -12,6 +12,7 @@ def index(request):
     return render(request, 'index/index.html')
 
 
+
 @csrf_exempt
 def audio(request):
     if request.method == 'POST':
@@ -22,7 +23,7 @@ def audio(request):
             with open('index/static/index/media/audio.wav', 'wb') as destination:
                 for chunk in audio_file.chunks():
                     destination.write(chunk)
-
+#test
             r = sr.Recognizer()
             with sr.AudioFile('index/static/index/media/audio.wav') as source:
                 audio_data = r.record(source)
@@ -33,6 +34,32 @@ def audio(request):
 
 
     return JsonResponse({'message': 'Audio received and saved successfully.'})
+
+# from transformers import pipeline
+# from transformers import AutoTokenizer, AutoModelForCausalLM
+#
+#
+#
+# def remove_duplicate_words(input_str):
+#     words = input_str.split()  # Разбиваем строку на слова
+#     unique_words = []  # Здесь будем хранить уникальные слова
+#     for word in words:
+#         if word not in unique_words:
+#             unique_words.append(word)
+#     return ' '.join(unique_words)
+# # Load model directly
+# prompt = 'при включении тумблера топливный насос вал топливоподкачивающего агрегата не вращается.контактор ктн не включается'
+#
+# tokenizer = AutoTokenizer.from_pretrained("arsenZabara/RJD-hak", Legacy=True)
+# model = AutoModelForCausalLM.from_pretrained("arsenZabara/RJD-hak")
+#
+# pipe = pipeline("text-generation", model=model, tokenizer=tokenizer, max_length=200)
+# result = pipe(f"{prompt}")
+#
+# temp = result[0]['generated_text'].replace(prompt, '').replace('.', ' ')
+# res = remove_duplicate_words((temp))
+#
+# print(res)
 
 
 
